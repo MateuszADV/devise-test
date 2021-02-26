6b5bb9d710973182ce2626d997840caaac06c936
MateuszADV <mateusz.adv@gmail.com>
2021-02-26 18:57:59 +0100

    add: role to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
     before_action :update_allowed_parameters, if: :devise_controller?
 
     def require_user_logged_in!
-        redirect_to new_user_session_path, alert: "Musisz być zalogowany" if current_user == nil
+        redirect_to new_user_session_path, alert: "Musisz być zalogowany" if !user_signed_in?
     end
 
     protected
     def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :name, :lastname, :password)}
-        devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :name, :lastname, :password,
+        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :name, :lastname, :role, :password)}
+        devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :name, :lastname, :role, :password,
             :current_password )}
 
     end
