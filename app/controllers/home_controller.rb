731b3d074f9844dc6bbe397f74f5f589d921265a
MateuszADV <mateusz.adv@gmail.com>
2021-04-02 11:56:32 +0200

    test Enum

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
+  require 'enums/Note_Enum'
   def index
+    # puts "TEST"
+    # puts NoteEnum.statuses
+    # puts NoteEnum.makings
+    @statuses = NoteEnum.statuses
+    @makings = NoteEnum.makings
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+    @makings.each do |n|
+      puts n[0]
+    end
+    puts "---------------------------------------------------"
+     @statuses.each do |s|
+      puts s[0]
+     end
+     puts "++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+    puts NoteEnum.imgs
   end
 end
---
+++ app/services/enums/note_enum.rb
@@ -0,0 +1,7 @@
+class NoteEnum < ApplicationRecord
+    # Enumerator
+    enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
+    enum making: {PAPIER: 0, POLIMER: 1, HYBRYDA: 2}
+    enum img: {SKAN: 0 , FOTO: 1 , LOC: 2 , WWW: 3 }
+  end
+  
