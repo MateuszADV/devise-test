d13b17b13d05fd7ea9bd647efbd018f5d9a72ccf
MateuszADV <mateusz.adv@gmail.com>
2021-04-05 13:36:34 +0200

    enum test015

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,6 +5,7 @@ class NotesController < ApplicationController
   # GET /notes or /notes.json
   def index
     @notes = Note.all
+    puts Element.statuses
   end
 
   # GET /notes/1 or /notes/1.json
@@ -14,7 +15,7 @@ class NotesController < ApplicationController
   # GET /notes/new
   def new
     @note = Note.new
-   
+    @statuses = Element.statuses
   end
 
   # GET /notes/1/edit
