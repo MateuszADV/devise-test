ebb7bdc2ad6c8bf9a8e2814468c16d36c4537b1e
MateuszADV <mateusz.adv@gmail.com>
2021-04-05 13:29:57 +0200

    enum test014

--- app/services/element.rb
+++ app/services/element.rb
@@ -2,7 +2,7 @@ class Element < ActiveRecord::Base
     # Enumerator
     enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
 
-    # enum making: {PAPIER: 0, POLIMER: 1, HYBRYDA: 2}
+    enum making: ["PAPIER", "POLIMER", "HYBRYDA"]
 
     # enum img: {SKAN: 0 , FOTO: 1 , LOC: 2 , WWW: 3 }
 
