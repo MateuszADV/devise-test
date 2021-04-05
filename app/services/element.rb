f2f7cea2af0fbd8f6683ac369bee92c72fe24b12
MateuszADV <mateusz.adv@gmail.com>
2021-04-05 13:13:43 +0200

    enum test008

---
+++ app/services/element.rb
@@ -0,0 +1,10 @@
+class NoteEnum
+    # Enumerator
+    enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
+
+    # enum making: {PAPIER: 0, POLIMER: 1, HYBRYDA: 2}
+
+    # enum img: {SKAN: 0 , FOTO: 1 , LOC: 2 , WWW: 3 }
+
+end
+  
