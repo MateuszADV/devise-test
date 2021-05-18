4a0277662362e742689944631c699ba46da0374b
MateuszADV <mateusz.adv@gmail.com>
2021-05-18 16:49:48 +0200

    Wybów języja

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale
     protect_from_forgery with: :exception
 
     before_action :update_allowed_parameters, if: :devise_controller?
@@ -12,8 +13,19 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :name, :lastname, :role, :password)}
         devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :name, :lastname, :role, :password,
             :current_password )}
-
     end
 
-    
+    private
+        def default_url_options
+            { locale: I18n.locale }
+        end
+
+        def set_locale
+            I18n.locale = extract_locale || I18n.default_locale
+        end
+
+        def extract_locale
+            parsed_locale = params[:locale]
+            I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
- 
-  resources :notes
-  get 'pecunia/index'
-  devise_for :users
-  # get 'home/index'
-  root 'home#index' 
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :notes
+    get 'pecunia/index'
+    devise_for :users
+    # get 'home/index'
+    root 'home#index' 
 
-  get 'pecunia/index', to: 'pecunia#index', as: 'pecunia'
+    get 'pecunia/index', to: 'pecunia#index', as: 'pecunia'
 
-  # TESTY
-  get 'test/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # TESTY
+    get 'test/index'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
